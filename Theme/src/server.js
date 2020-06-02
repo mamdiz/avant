0cd84554ab9016448c1f0dcac8c8e7914953f793
Hesam Kalhor <hesamkal2009@gmail.com>
2020-06-02 09:23:09 +0430

    server.js and some packages added

--- Theme/src/server.js
+++ Theme/src/server.js
@@ -5,13 +5,10 @@ var app = express();
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
 
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
 const Router = require("./routes/router");
 
 app.use("/", Router);
 
-app.listen(8080, function() {
+app.listen(5000, function() {
   console.log("Server is listening at 8080");
 });
